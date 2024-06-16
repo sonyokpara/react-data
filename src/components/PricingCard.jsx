a827e6c1a8737bc5fde8d3a8e6fc9a22249c5e9d
sonyokpara <sony.okpara@yahoo.com>
2024-06-16 19:00:38 +0000

    Complete the pricing section

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -4,15 +4,19 @@ import { FaCircleCheck } from "react-icons/fa6";
 const PricingCard = (props) => {
   const Icon = props.icon;
   return (
-    <div className="flex flex-col justify-between  bg-white border border-slate-200 rounded-xl p-5 shadow-lg">
+    <div
+      className={`flex flex-col justify-between gap-6 ${
+        props.selected ? "bg-gray-100" : "bg-white"
+      } border border-slate-200 rounded-xl p-5 shadow-md`}
+    >
       <div className="w-[300px] flex flex-col items-center">
         <div className="w-14 h-14 flex items-center justify-center rounded-full border border-gray-200">
           <Icon className="w-10 h-10 text-black" />
         </div>
-        <h2 className="text-xl md:text-3xl font-open-sans font-semibold text-black">
+        <h2 className="text-lg my-2 md:text-2xl font-montserrat font-semibold text-black">
           {props.title}
         </h2>
-        <h3 className="text-2xl my-2 font-semibold text-primary font-bond font-montserrat">
+        <h3 className="text-2xl my-2 font-semibold text-black font-bond font-montserrat">
           ${props.price}
         </h3>
         <ul className="text-black/90 space-y-3">
@@ -28,8 +32,8 @@ const PricingCard = (props) => {
       <div className="flex justify-center items-center p-3">
         <Button
           label="Order Now"
-          backgroundColor="bg-black"
-          textColor="tex-primary"
+          backgroundColor="bg-primary"
+          textColor="text-black"
         />
       </div>
     </div>
